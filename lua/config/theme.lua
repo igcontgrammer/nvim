f5f9bd277b4efdc2a6564e6a797bcf947cef16a3
igcontgrammer <ignacio.sanmartin.lemus@gmail.com>
2024-10-21 01:40:59 -0300

    feat: theme details

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -1,13 +1,69 @@
 local M = {}
 
 function M.setup()
-	function SetTheme(color)
-		color = color or "rose-pine"
-		vim.cmd.colorscheme(color)
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	end
-	SetTheme()
+	require("rose-pine").setup({
+		variant = "main",
+		dark_variant = "main",
+		dim_inactive_windows = false,
+		extend_background_behind_borders = true,
+		enable = {
+			terminal = true,
+			legacy_highlights = true,
+			migrations = true,
+		},
+		styles = {
+			bold = false,
+			italic = false,
+			transparency = true,
+		},
+		groups = {
+			border = "muted",
+			link = "iris",
+			panel = "surface",
+
+			error = "love",
+			hint = "iris",
+			info = "foam",
+			note = "pine",
+			todo = "rose",
+			warn = "gold",
+
+			git_add = "foam",
+			git_change = "rose",
+			git_delete = "love",
+			git_dirty = "rose",
+			git_ignore = "muted",
+			git_merge = "iris",
+			git_rename = "pine",
+			git_stage = "iris",
+			git_text = "rose",
+			git_untracked = "subtle",
+
+			h1 = "iris",
+			h2 = "foam",
+			h3 = "rose",
+			h4 = "gold",
+			h5 = "pine",
+			h6 = "foam",
+		},
+		palette = {
+			moon = {
+				base = "#14191a",
+				overlay = "#363738",
+			},
+		},
+		highlight_groups = {
+			Comment = { fg = "#6c6f93" },
+			CursorLine = { bg = "#1a1b26" },
+			CursorColumn = { bg = "#1a1b26" },
+			["@variable"] = { fg = "#c0caf5" },
+			["@variable.member"] = { fg = "#c0caf5" },
+			["@constant"] = { fg = "#bb9af7", bold = true, italic = true },
+			["@constant.member"] = { fg = "#c0caf5", bold = true, italic = true },
+		},
+	})
+
+	vim.cmd("colorscheme rose-pine-moon")
 end
 
 return M
